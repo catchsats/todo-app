5c455f246414fc5413266cad57ff0ee01a7cbd34
Lenovo <catchsats@gmail.com>
2026-05-30 08:18:41 +0530

    Button added

--- gui.py
+++ gui.py
@@ -4,6 +4,8 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip = 'Enter todo')
 add_button = sg.Button('Add')
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box], [add_button]],
+                   font = ('Helvetica', 20))
 window.read()
 window.close()
