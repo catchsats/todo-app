bf9b351adae4d207d32ff4295e4a13d977135d1e
Lenovo <catchsats@gmail.com>
2026-05-31 20:38:05 +0530

    Added exit, remove button

--- gui.py
+++ gui.py
@@ -5,10 +5,16 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip = 'Enter todo', key='todo')
 add_button = sg.Button('Add')
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
+
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font = ('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -37,6 +43,16 @@ while True:
             index = todos.index(old_value[0])
             todos[index] = new_value
             window['todos'].update(values=todos)
+        case 'Complete':
+            todos = functions.get_todos()
+            old_value = values['todos']
+            index = todos.index(old_value[0])
+            todos.pop(index)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'Exit':
+            exit()
+
         case sg.WIN_CLOSED:
             break
 window.close()
